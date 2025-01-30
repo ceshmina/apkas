af263d443db28a49bd26c2cb8942cd60163466a4
ceshmina <ceshmina@gmail.com>
2025-01-30 17:54:41 +0900

    Change OIDC condition

--- terraform/modules/apkas/github.tf
+++ terraform/modules/apkas/github.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "github_assume_role" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values   = ["repo:ceshmina/apkas:*"]
     }
