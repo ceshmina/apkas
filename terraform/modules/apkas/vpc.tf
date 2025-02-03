21193403e1f2b7791c087e548ba8cba4dc8d2c59
ceshmina <ceshmina@gmail.com>
2025-02-03 23:28:25 +0900

    Enable Data API, modify settings

--- terraform/modules/apkas/vpc.tf
+++ terraform/modules/apkas/vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "this" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -8,7 +8,7 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "private_1" {
-  vpc_id            = aws_vpc.vpc.id
+  vpc_id            = aws_vpc.this.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = var.aws_availability_zones[0]
   tags = {
@@ -17,7 +17,7 @@ resource "aws_subnet" "private_1" {
 }
 
 resource "aws_subnet" "private_2" {
-  vpc_id            = aws_vpc.vpc.id
+  vpc_id            = aws_vpc.this.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = var.aws_availability_zones[1]
   tags = {
