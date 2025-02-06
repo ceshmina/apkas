f56e1aa4080cbde8a2ee99caed5674c805eac5a8
ceshmina <ceshmina@gmail.com>
2025-02-07 00:16:34 +0900

    Set env params on App Runner

--- terraform/modules/apkas/apprunner.tf
+++ terraform/modules/apkas/apprunner.tf
@@ -18,6 +18,35 @@ resource "aws_iam_role_policy_attachment" "apprunner" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }
 
+data "aws_iam_policy_document" "apprunner_instance" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["tasks.apprunner.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "apprunner_instance" {
+  name               = "apprunner-api-instance"
+  assume_role_policy = data.aws_iam_policy_document.apprunner_instance.json
+}
+
+data "aws_iam_policy_document" "apprunner_secrets" {
+  statement {
+    sid       = "ForGetSecretValue"
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = [aws_secretsmanager_secret.aurora_user.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "apprunner_secrets" {
+  name   = "apprunner-secrets"
+  role   = aws_iam_role.apprunner_instance.name
+  policy = data.aws_iam_policy_document.apprunner_secrets.json
+}
+
 resource "aws_apprunner_service" "api" {
   service_name = var.api_apprunner_service_name
   source_configuration {
@@ -26,6 +55,15 @@ resource "aws_apprunner_service" "api" {
       image_identifier      = "${aws_ecr_repository.api.repository_url}:latest"
       image_configuration {
         port = "8000"
+        runtime_environment_variables = {
+          DB_HOST = aws_rds_cluster.aurora.endpoint
+          DB_PORT = 5432
+          DB_NAME = "apkas"
+        }
+        runtime_environment_secrets = {
+          DB_USER     = "${aws_secretsmanager_secret_version.aurora_user_value.arn}:username::"
+          DB_PASSWORD = "${aws_secretsmanager_secret_version.aurora_user_value.arn}:password::"
+        }
       }
     }
     authentication_configuration {
@@ -34,8 +72,9 @@ resource "aws_apprunner_service" "api" {
   }
   auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.api.arn
   instance_configuration {
-    cpu    = "256"
-    memory = "512"
+    cpu               = "256"
+    memory            = "512"
+    instance_role_arn = aws_iam_role.apprunner_instance.arn
   }
 }
 
