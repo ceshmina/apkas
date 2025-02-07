3f4a84298394f7e0927eed2edc1cbab11534816c
ceshmina <ceshmina@gmail.com>
2025-02-07 22:28:29 +0900

    Add tags

--- api/app/web/route.py
+++ api/app/web/route.py
@@ -9,22 +9,22 @@ index_controller = IndexController()
 v1_controller = V1Controller()
 
 
-@router.get('/', response_model=IndexResponse)
+@router.get('/', response_model=IndexResponse, tags=['healthcheck'])
 def get_index() -> IndexResponse:
     return index_controller.get_index()
 
 
-@router.get('/v1/diary/{diary_id}', response_model=DiaryResponse)
+@router.get('/v1/diary/{diary_id}', response_model=DiaryResponse, tags=['v1'])
 def get_diary(diary_id: int) -> DiaryResponse:
     return v1_controller.get_diary(diary_id)
 
 
-@router.get('/v1/location/{location_id}', response_model=LocationResponse)
+@router.get('/v1/location/{location_id}', response_model=LocationResponse, tags=['v1'])
 def get_location(location_id: int) -> LocationResponse:
     return v1_controller.get_location(location_id)
 
 
-@router.get('/v1/diaries/search', response_model=SearchDiariesResponse, response_model_exclude_unset=True)
+@router.get('/v1/diaries/search', response_model=SearchDiariesResponse, response_model_exclude_unset=True, tags=['v1'])
 def search_diaries(
     date: str | None = None, month: str | None = None, location_id: int | None = None
 ) -> SearchDiariesResponse:
