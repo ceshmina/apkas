cdcbedca273a675654f6459b94af3b416e04bb82
ceshmina <ceshmina@gmail.com>
2025-02-07 22:25:03 +0900

    Routes

--- api/app/web/route.py
+++ api/app/web/route.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from web.controller.index import IndexController, IndexResponse
-from web.controller.v1 import DiaryResponse, V1Controller
+from web.controller.v1 import DiaryResponse, LocationResponse, SearchDiariesResponse, V1Controller
 
 router = APIRouter()
 
@@ -17,3 +17,15 @@ def get_index() -> IndexResponse:
 @router.get('/v1/diary/{diary_id}', response_model=DiaryResponse)
 def get_diary(diary_id: int) -> DiaryResponse:
     return v1_controller.get_diary(diary_id)
+
+
+@router.get('/v1/location/{location_id}', response_model=LocationResponse)
+def get_location(location_id: int) -> LocationResponse:
+    return v1_controller.get_location(location_id)
+
+
+@router.get('/v1/diaries/search', response_model=SearchDiariesResponse, response_model_exclude_unset=True)
+def search_diaries(
+    date: str | None = None, month: str | None = None, location_id: int | None = None
+) -> SearchDiariesResponse:
+    return v1_controller.search_diaries(date, month, location_id)
