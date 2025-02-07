fb41121889e41ac6658c2c5941d48697254b8669
ceshmina <ceshmina@gmail.com>
2025-02-07 22:18:22 +0900

    Controller

--- api/app/web/route.py
+++ api/app/web/route.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from web.controller.index import IndexController, IndexResponse
-from web.controller.v1 import GetDiaryResponse, V1Controller
+from web.controller.v1 import DiaryResponse, V1Controller
 
 router = APIRouter()
 
@@ -14,6 +14,6 @@ def get_index() -> IndexResponse:
     return index_controller.get_index()
 
 
-@router.get('/v1/diary/{diary_id}', response_model=GetDiaryResponse)
-def get_diary(diary_id: int) -> GetDiaryResponse:
+@router.get('/v1/diary/{diary_id}', response_model=DiaryResponse)
+def get_diary(diary_id: int) -> DiaryResponse:
     return v1_controller.get_diary(diary_id)
