62378d043ea9b0e3496ed4f8a588292c66180b27
ceshmina <shu@apkas.net>
2025-08-28 09:18:07 +0900

    日記表示のテスト

---
+++ frontend/model/diary.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from 'bun:test'
+
+import { Diary } from './diary'
+
+
+describe('有効な日記の判定', () => {
+  test('有効な作成時刻を判定できる', () => {
+    const diary = new Diary('', new Date('2025-01-01T21:00:00+09:00'))
+    expect(diary.isValid()).toBe(true)
+  })
+
+  test('無効な作成時刻を判定できる', () => {
+    const diary = new Diary('', new Date(''))
+    expect(diary.isValid()).toBe(false)
+  })
+})
+
+
+describe('作成時刻のフォーマット', () => {
+  test('作成時刻を指定したフォーマットで表示できる', () => {
+    const diary = new Diary('', new Date('2025-01-01T21:00:00+09:00'))
+    expect(diary.getDate()).toBe('2025年1月1日')
+  })
+
+  test('作成時刻を日本のタイムゾーンで表示できる', () => {
+    const diary = new Diary('', new Date('2025-01-02T05:00:00+09:00'))
+    expect(diary.getDate()).toBe('2025年1月2日')
+  })
+})
