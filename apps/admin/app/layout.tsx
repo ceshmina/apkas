2258c4e189bff78ae729573203d63e9207919a69
ceshmina <shu@apkas.net>
2025-09-07 17:18:38 +0900

    @apkas/adminを作成

---
+++ apps/admin/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Noto_Sans_JP, Source_Code_Pro } from 'next/font/google'
+
+import './globals.css'
+
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-noto-sans-jp',
+})
+
+const sourceCodePro = Source_Code_Pro({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-source-code-pro',
+})
+
+
+export const metadata: Metadata = {
+  title: 'apkas',
+}
+
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (<html lang="ja">
+    <body className={`${notoSansJP.variable} ${sourceCodePro.variable}`}>
+      {children}
+    </body>
+  </html>)
+}
