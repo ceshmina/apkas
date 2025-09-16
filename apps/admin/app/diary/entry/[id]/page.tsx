555b46e3cf18ada7993deb883e554751bbe6df28
ceshmina <shu@apkas.net>
2025-09-16 11:31:58 +0900

    本文を編集できる

--- apps/admin/app/diary/entry/[id]/page.tsx
+++ apps/admin/app/diary/entry/[id]/page.tsx
@@ -1,5 +1,4 @@
-import Markdown from 'react-markdown'
-
+import Markdown from '@/components/markdown'
 import { formatDiaryTitle, getDiaryByID } from '@/core/diary'
 
 
@@ -17,14 +16,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
 
       <section className="my-4">
         <div className="my-2">
-          <Markdown
-            components={{
-              h2: ({ children }) => (<h2 className="text-lg font-bold mt-4 mb-2">{children}</h2>),
-              p: ({ children }) => (<p className="text-sm font-normal my-1">{children}</p>),
-              ul: ({ children }) => (<ul className="my-2 pl-5">{children}</ul>),
-              li: ({ children }) => (<li className="text-sm font-normal my-1 list-disc">{children}</li>),
-            }}
-          >{diary.content}</Markdown>
+          <Markdown>{diary.content}</Markdown>
         </div>
       </section>
     </main>
--- apps/admin/app/diary/new/page.tsx
+++ apps/admin/app/diary/new/page.tsx
@@ -2,10 +2,15 @@
 
 import { useState } from 'react'
 
+import Markdown from '@/components/markdown'
+
 
 export default function Home() {
   const [title, setTitle] = useState('新規')
   const [isTitleEditing, setIsTitleEditing] = useState(false)
+
+  const [content, setContent] = useState('新しい日記です。')
+  const [isContentEditing, setIsContentEditing] = useState(false)
 
   return (<div>
     <main className="p-4">
@@ -34,6 +39,40 @@ export default function Home() {
             >編集</button>
           </div>
         }
+      </section>
+
+      <section className="my-8">
+        <div className="flex">
+          <button
+            className={`
+              border-[1px] border-b-0 border-gray-300 rounded-t px-2 py-1 text-sm mr-2
+              ${isContentEditing ? 'font-bold' : 'bg-gray-100'}
+            `}
+            onClick={() => setIsContentEditing(true)}
+          >編集</button>
+          <button
+            className={`
+              border-[1px] border-b-0 border-gray-300 rounded-t px-2 py-1 text-sm
+              ${isContentEditing ? 'bg-gray-100' : 'font-bold'}
+            `}
+            onClick={() => setIsContentEditing(false)}
+          >プレビュー</button>
+        </div>
+        <div className="border-[1px] border-gray-300 rounded-b p-4">
+          {isContentEditing
+          ? <div>
+              <textarea
+                rows={20}
+                className="w-[100%] text-sm p-2"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+              />
+            </div>
+          : <div>
+              <Markdown>{content}</Markdown>
+            </div>
+          }
+        </div>
       </section>
     </main>
   </div>)
