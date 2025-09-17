b9d223c60d6a322879955f4a2cf088d0f45acefc
ceshmina <shu@apkas.net>
2025-09-17 09:07:54 +0900

    GitHub Actionsでadminビルド

--- terraform/modules/apkas/iam.tf
+++ terraform/modules/apkas/iam.tf
@@ -61,6 +61,35 @@ resource "aws_iam_policy" "sync_s3" {
   policy = data.aws_iam_policy_document.sync_s3.json
 }
 
+data "aws_iam_policy_document" "push_ecr" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+    ]
+    resources = [
+      aws_ecr_repository.admin.arn,
+    ]
+  }
+}
+
+resource "aws_iam_policy" "push_ecr" {
+  name   = "push-ecr"
+  policy = data.aws_iam_policy_document.push_ecr.json
+}
+
 resource "aws_iam_role" "github" {
   name = "github-actions"
   assume_role_policy = jsonencode({
@@ -88,6 +117,11 @@ resource "aws_iam_role_policy_attachment" "github_dynamodb" {
 resource "aws_iam_role_policy_attachment" "github_s3" {
   role       = aws_iam_role.github.name
   policy_arn = aws_iam_policy.sync_s3.arn
+}
+
+resource "aws_iam_role_policy_attachment" "github_ecr" {
+  role       = aws_iam_role.github.name
+  policy_arn = aws_iam_policy.push_ecr.arn
 }
 
 resource "aws_iam_role" "admin" {
