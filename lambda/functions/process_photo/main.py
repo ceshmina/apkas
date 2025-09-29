af2ad8f854c39e74c52f686bb07c417b793ecd32
ceshmina <shu@apkas.net>
2025-09-29 22:07:24 +0900

    EXIFをDynamoDBに格納

--- lambda/functions/process_photo/main.py
+++ lambda/functions/process_photo/main.py
@@ -10,6 +10,7 @@ from app.core.process_photo import process
 
 
 s3_client = boto3.client('s3')
+dynamodb_client = boto3.client('dynamodb')
 
 
 def handler(event: S3Event, context: Context) -> dict[str, Any]:
@@ -32,11 +33,42 @@ def handler(event: S3Event, context: Context) -> dict[str, Any]:
     s3_client.download_file(Bucket=source_bucket, Key=source_key, Filename=input_path)
 
     target_dir = f'/tmp/output/{photo_id}'
-    _ = process(input_path, target_dir)
+    metadata = process(input_path, target_dir)
+
+    dynamodb_item = {
+        'pid': { 'S': 'ready' },
+        'sid': { 'S': f'photo#{str(photo_id)}' },
+        'item_type': { 'S': 'photo' },
+        'created_at': { 'S': metadata.created_at.isoformat() },
+        'paths': { 'M': {
+            'original': { 'S': f's3://{source_bucket}/{source_key}' },
+        }},
+        'metadata': { 'M': {} },
+    }
+    if metadata.make:
+        dynamodb_item['metadata']['M']['make'] = { 'S': metadata.make }
+    if metadata.model:
+        dynamodb_item['metadata']['M']['model'] = { 'S': metadata.model }
+    if metadata.lens_model:
+        dynamodb_item['metadata']['M']['lens_model'] = { 'S': metadata.lens_model }
+    if metadata.focal_length:
+        dynamodb_item['metadata']['M']['focal_length'] = { 'N': str(metadata.focal_length) }
+    if metadata.focal_length_in_35mm_film:
+        dynamodb_item['metadata']['M']['focal_length_in_35mm_film'] = { 'N': str(metadata.focal_length_in_35mm_film) }
+    if metadata.f_number:
+        dynamodb_item['metadata']['M']['f_number'] = { 'N': str(metadata.f_number) }
+    if metadata.exposure_time:
+        dynamodb_item['metadata']['M']['exposure_time'] = { 'N': str(metadata.exposure_time) }
+    if metadata.iso_speed_ratings:
+        dynamodb_item['metadata']['M']['iso_speed_ratings'] = { 'N': str(metadata.iso_speed_ratings) }
 
     output_files = os.listdir(target_dir)
     for filename in output_files:
         upload_key = f'{str(photo_id)}/{filename}'
         s3_client.upload_file(Filename=os.path.join(target_dir, filename), Bucket='apkas-development-photos', Key=upload_key)
+        dynamodb_key = filename.split('.')[0]
+        dynamodb_item['paths']['M'][dynamodb_key] = { 'S': f's3://apkas-development-photos/{upload_key}' }
+
+    dynamodb_client.put_item(TableName='diary', Item=dynamodb_item)
 
     return { 'message': f'ファイル {source_key}を処理しました' }
